8c38e871fe908d74c75c7571e0f00c486addec26
muhammadh9 <m.huzaif999@gmail.com>
2024-05-30 01:07:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ function addTodo() {
     var todoParent = document.getElementById("todoParent")
     var Input = document.getElementById("Input")
     if (Input.value.length < 3) {
-        alert("Enter correct todo value")
+        alert("Enter correct list value")
         return
     }
     var li = document.createElement("li")
     li.innerHTML = Input.value
     var editBtn = document.createElement("button")
     editBtn.innerHTML = "EDIT"
+    editBtn.setAttribute("onclick", "editTodo(this)")
 
     var deleteBtn = document.createElement("button")
     deleteBtn.innerHTML = "DELETE"
@@ -20,6 +21,7 @@ function addTodo() {
     Input.value = ""
 }
 
+
 function deleteAll() {
     var todoParent = document.getElementById("todoParent")
     console.log(todoParent.innerHTML)
@@ -29,3 +31,14 @@ function delTodo(ele) {
     console.log("delTodo", ele.parentNode)
     ele.parentNode.remove()
 }   
+function editTodo(ele) {
+    var editVal = prompt("Enter value", ele.previousSibling.nodeValue)
+    console.log(editVal)
+    ele.previousSibling.nodeValue = editVal
+    if (Input == Input) {
+        alert("nice")
+    }
+          
+}
+
+
